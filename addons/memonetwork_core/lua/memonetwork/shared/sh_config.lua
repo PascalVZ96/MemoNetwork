28753b3d756f4b393e08f3cfc490569af86e73be
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-06-28 18:24:22 +0200

    Alpha 11.1: bump version

--- addons/memonetwork_core/lua/memonetwork/shared/sh_config.lua
+++ addons/memonetwork_core/lua/memonetwork/shared/sh_config.lua
@@ -5,7 +5,7 @@ MemoNetwork.Config = MemoNetwork.Config or {}
 
 MemoNetwork.Config.ServerName = "MemoNetwork"
 MemoNetwork.Config.Subtitle = "Industrial Sandbox"
-MemoNetwork.Config.Version = "Alpha 10.3-dev"
+MemoNetwork.Config.Version = "Alpha 11.1-dev"
 
 MemoNetwork.Config.Website = "https://memocraft.nl"
 MemoNetwork.Config.Discord = "https://memocraft.nl/?c=Discord"
