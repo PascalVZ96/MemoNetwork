20c949fe7457c4990e64a91c4d76e6f9baee3b70
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-06-30 20:34:26 +0200

    Alpha 20: load Admin Suite

--- addons/memonetwork_core/lua/autorun/memonetwork_loader.lua
+++ addons/memonetwork_core/lua/autorun/memonetwork_loader.lua
@@ -1,4 +1,4 @@
--- MemoNetwork Alpha 19.2 Loader
+-- MemoNetwork Alpha 20 Loader
 
 MemoNetwork = MemoNetwork or {}
 
@@ -51,6 +51,7 @@ AddClient("memonetwork/client/cl_spectate_hud.lua")
 AddClient("memonetwork/client/cl_build_assistant.lua")
 AddClient("memonetwork/client/cl_build_manager.lua")
 AddClient("memonetwork/client/cl_build_tools.lua")
+AddClient("memonetwork/client/cl_admin_suite.lua")
 AddClient("memonetwork/client/cl_admin_dashboard2.lua")
 AddClient("memonetwork/client/cl_control_center.lua")
 AddClient("memonetwork/client/cl_map_manager.lua")
