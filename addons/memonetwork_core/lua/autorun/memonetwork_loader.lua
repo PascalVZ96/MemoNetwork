00f1a1f373c19d217feb1fdca9bfeeeb12e354ee
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-07-01 17:24:50 +0200

    Alpha 24: load Build Editor

--- addons/memonetwork_core/lua/autorun/memonetwork_loader.lua
+++ addons/memonetwork_core/lua/autorun/memonetwork_loader.lua
@@ -1,4 +1,4 @@
--- MemoNetwork Alpha 23 Loader
+-- MemoNetwork Alpha 24 Loader
 
 MemoNetwork = MemoNetwork or {}
 
@@ -55,6 +55,7 @@ AddClient("memonetwork/client/cl_admin_suite.lua")
 AddClient("memonetwork/client/cl_world_manager.lua")
 AddClient("memonetwork/client/cl_intelligence.lua")
 AddClient("memonetwork/client/cl_command_center.lua")
+AddClient("memonetwork/client/cl_build_editor.lua")
 AddClient("memonetwork/client/cl_admin_dashboard2.lua")
 AddClient("memonetwork/client/cl_control_center.lua")
 AddClient("memonetwork/client/cl_map_manager.lua")
