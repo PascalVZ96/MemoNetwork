34466284f462ae0c3a957742cb827f7e34286703
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-07-01 16:52:17 +0200

    Alpha 22: bump version

--- addons/memonetwork_core/lua/memonetwork/shared/sh_config.lua
+++ addons/memonetwork_core/lua/memonetwork/shared/sh_config.lua
@@ -5,7 +5,7 @@ MemoNetwork.Config = MemoNetwork.Config or {}
 
 MemoNetwork.Config.ServerName = "MemoNetwork"
 MemoNetwork.Config.Subtitle = "Industrial Sandbox"
-MemoNetwork.Config.Version = "Alpha 21.0-dev"
+MemoNetwork.Config.Version = "Alpha 22.0-dev"
 
 MemoNetwork.Config.Website = "https://memocraft.nl"
 MemoNetwork.Config.Discord = "https://memocraft.nl/?c=Discord"
